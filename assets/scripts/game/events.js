92ef4e431cd2762743d76dd18d631ed66e86f3f5
ks603 <kevin.shaughnessy18@gmail.com>
2020-02-13 18:06:47 -0500

    Updated end of game for Win or tie
    made sure game ended

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -8,7 +8,6 @@ const onGetGames = function () {
     .then(ui.onGetGameSuccess)
     .catch(ui.onGetGameFailure)
 }
-
 const onCreateGame = function (data) {
   api.createGame(data)
     .then(ui.onCreateGameSuccess)
@@ -31,52 +30,62 @@ $('.box').on('click', function (event) {
 
       if ($('#0').html() === 'X' && $('#1').html() === 'X' && $('#2').html() === 'X') {
         $('#message').text('Player X won')
+        $('#gameBoard').hide()
       } else if ($('#3').html() === 'X' && $('#4').html() === 'X' && $('#5').html() === 'X') {
         $('#message').text('Player X won')
-        event.clear()
+        $('#gameBoard').hide()
       } else if ($('#6').html() === 'X' && $('#7').html() === 'X' && $('#8').html() === 'X') {
         $('#message').text('Player X won')
-        event.clear()
+        $('#gameBoard').hide()
       } else if ($('#0').html() === 'X' && $('#3').html() === 'X' && $('#6').html() === 'X') {
         $('#message').text('Player X won')
-        event.clear()
+        $('#gameBoard').hide()
       } else if ($('#1').html() === 'X' && $('#4').html() === 'X' && $('#7').html() === 'X') {
         $('#message').text('Player X won')
-        event.clear()
+        $('#gameBoard').hide()
       } else if ($('#2').html() === 'X' && $('#5').html() === 'X' && $('#8').html() === 'X') {
         $('#message').text('Player X won')
-        event.clear()
+        $('#gameBoard').hide()
       } else if ($('#0').html() === 'X' && $('#4').html() === 'X' && $('#8').html() === 'X') {
         $('#message').text('Player X won')
-        event.clear()
+        $('#gameBoard').hide()
       } else if ($('#2').html() === 'X' && $('#4').html() === 'X' && $('#6').html() === 'X') {
         $('#message').text('Player X won')
-        event.clear()
+        $('#gameBoard').hide()
       }
       if ($('#0').html() === 'O' && $('#1').html() === 'O' && $('#2').html() === 'O') {
         $('#message').text('Player O won')
-        event.clear()
+        $('#gameBoard').hide()
       } else if ($('#3').html() === 'O' && $('#4').html() === 'O' && $('#5').html() === 'O') {
         $('#message').text('Player O won')
-        event.clear()
+        $('#gameBoard').hide()
       } else if ($('#6').html() === 'O' && $('#7').html() === 'O' && $('#8').html() === 'O') {
         $('#message').text('Player O won')
-        event.clear()
+        $('#gameBoard').hide()
       } else if ($('#0').html() === 'O' && $('#3').html() === 'O' && $('#6').html() === 'O') {
         $('#message').text('Player O won')
-        event.clear()
+        $('#gameBoard').hide()
       } else if ($('#1').html() === 'O' && $('#4').html() === 'O' && $('#7').html() === 'O') {
         $('#message').text('Player O won')
-        event.clear()
+        $('#gameBoard').hide()
       } else if ($('#2').html() === 'O' && $('#5').html() === 'O' && $('#8').html() === 'O') {
         $('#message').text('Player O won')
-        event.clear()
+        $('#gameBoard').hide()
       } else if ($('#0').html() === 'O' && $('#4').html() === 'O' && $('#8').html() === 'O') {
         $('#message').text('Player O won')
-        event.clear()
+        $('#gameBoard').hide()
       } else if ($('#2').html() === 'O' && $('#4').html() === 'O' && $('#6').html() === 'O') {
         $('#message').text('player O won!')
-        event.clear()
+        $('#gameBoard').hide()
+      } else {
+        let count = 0
+        for (let i = 0; i < 9; i++) {
+          if ($('#' + i).html() !== '') {
+            count++
+          }
+        } if (count === 9) {
+          $('#message').text('its a tie!')
+        }
       }
     })
     .catch(ui.onUpdateGameFailure)
